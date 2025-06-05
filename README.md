a3dc9f199781363b8f233a2d5e797b817c1763ce
Wandyl26 <nepodiaz2@hotmail.com>
2025-06-05 13:17:40 -0500

    no message

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MobileChallengeUala
 
+
+Para la solución del problema decidí implementar un Splash donde descargo toda la información con Retrofit y la almaceno en una BD con Room.
+
+Esto me ayudo para implementar un Search Bar con un Lazy Column, ya que con esto puedo consultar el nombre de las ciudades y mostrar en pantalla 
+el mapa de Google con ubicación cuando se da click en el item de la ciudad. También el Lazy Column me permitió implementar un IconButtom 
+con el cual puedo manejar un estado de colores de azul y gris, dependiendo de si la ciudad es favorita o no. 
+También me permite captar cuando se da Click y colocar la ciudad en el Item que se selecciona como favorita.
+
+Utilice NavController para el flujo vertical y lanzar el flujo horizontal, dependiendo de la orientación que se detecte de la pantalla.
+
+La  pantalla de información no la vi necesaria ya que cuando se lanza la ubicación en el mapa, 
+se muestra esta información que se pedía en el Marker que se utiliza.
--- app/src/main/java/com/example/mobilechallengeuala/view/composable/MapsScreen.kt
+++ app/src/main/java/com/example/mobilechallengeuala/view/composable/MapsScreen.kt
@@ -10,6 +10,29 @@ import com.example.mobilechallengeuala.model.domain.CityDomain
 import com.example.mobilechallengeuala.viewmodel.SearchCitiesViewModel
 
 
+
+
+/*
+Elegí LazyColumn debido a que es la opción más robusta y recomendada para crear listas en Jetpack Compose,
+especialmente cuando se manejan grandes conjuntos de datos o listas con desplazamiento vertical.
+A diferencia de un Column normal, que renderiza todos los elementos de la lista de forma simultánea,
+LazyColumn solo renderiza los elementos que están visibles en pantalla. Esto optimiza el rendimiento y
+la eficiencia al usar menos memoria y recursos de CPU.
+
+Lo elegí principalmente por su rendimiento:
+
+Solo carga los elementos que se muestran en pantalla, lo que reduce el consumo de recursos y la demora en la carga de listas grandes,
+y esto me ayuda mucho debido a que hay mas de 200 mil registros de ciudades en la base de datos con una unica tabla,
+y el search bar consulta solo en esa tabla, aunque coloque un limite de 20 registros por consulta, por lo tanto
+me preocupaba el futuro donde se necesitara ampliar ese limite.
+
+
+El search bar lo elegí por comodidad, ya que me parece mas sencillo para establecer una búsqueda sensible donde se vaya actualizando
+o buscando a medida que escribe.
+
+*/
+
+
 @Composable
 fun SearchedCitiesNavigation(
     searchCitiesViewModel: SearchCitiesViewModel,
